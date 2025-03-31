60ef172e41788136afe1177e07b944a93e26a5a1
tupaken <legendazx@gmail.com>
2025-03-31 23:46:39 +0200

    Exam

--- main.c
+++ main.c
@@ -4,16 +4,14 @@
 #include <avr/interrupt.h>
 #include <avr/sleep.h>
 
-// Глобальные переменные
 volatile uint8_t zustand_sekunds = 0b000000;
 volatile uint8_t zustand_hours   = 0b00000;
-// СДЕЛАЙ
 volatile uint8_t zustand_minutes = 0b00000;
 volatile uint8_t sleepflag       = 0;
 volatile uint8_t brightness      = 128;
 
-// Прерывание таймера 2 (CTC) — считает секунды
-ISR(TIMER2_COMPA_vect){
+
+ISR(TIMER2_COMPA_vect){ //Iteraptor 
     if(zustand_sekunds == 0b111100){ // 0b111100
         Time_addierung();
         if (!sleepflag)
@@ -27,7 +25,6 @@ ISR(TIMER2_COMPA_vect){
     }
 }
 
-// Отрисовка на светодиодах
 void LEDS(){
     // Ledsanschlatung Minuten
     PORTB = (PORTB & ~(0b111111 << PB0))
@@ -44,7 +41,7 @@ void LEDS(){
           | ((zustand_hours << PC1) & 0xFF);
 };
 
-// Логика прибавления минут
+// Minutes Logik
 void Time_addierung(){
     if (zustand_minutes == 0b111100){
         zustand_minutes = 0b000000;
@@ -55,17 +52,17 @@ void Time_addierung(){
     }
 };
 
-// Инициализация таймера 2 (асинхрон, кварц)
 void timer2_init() {
-    ASSR |= (1 << AS2);
-    TCCR2A |= (1 << WGM01);        // Режим CTC (сброс по совпадению)
-    TCCR2B |= (1 << CS22) | (1 << CS20); // Предделитель 1024
-    OCR2A   = 255;                // Подстроено под ~1 сек при 1 МГц
-    TIMSK2 |= (1 << OCIE2A);      // Включаем прерывание по совпадению
-    sei();                        // Включаем глобальные прерывания
+    ASSR |= (1 << AS2); //Asyncrone modus aktiviert
+    TCCR2A |= (1 << WGM01);        //Clear Timer on Compare Match
+    TCCR2B |= (1 << CS22) | (1 << CS20); // Setztprescale auf 1024
+    OCR2A   = 255;                // zählt bis 31 OCR2A= (32768/1024)-1
+    TIMSK2 |= (1 << OCIE2A);      // Aktiviert Iterapt 
+    while (ASSR & ((1<<TCN2UB)|(1<<OCR2AUB)|(1<<TCR2AUB)|(1<<TCR2BUB))); // register syncronisieren
+    sei();                        // Iterapts Global aktivieren
 }
 
-// Проверка часов — прибавление часа
+
 void check_hours(){
     if (zustand_hours == 0b10111){
         zustand_hours = 0b00000;
@@ -75,7 +72,6 @@ void check_hours(){
     }
 }
 
-// Тестовое включение светодиодов (Minutes/Hours)
 void start_test(){
     //Test Minutes Leds
     PORTD |= (0b1 << PD0);
@@ -114,8 +110,9 @@ void start_test(){
     confirmation();
 }
 
-// «Подтверждение» — массовое вкл/выкл
+// startup blink
 void confirmation(){
+    //Ledsanschatung Minutes
     PORTB |= (PORTB & ~(0b111111 << PB0)
              | ((0b111111 >> 2 & 1) << PB0)
              | ((0b111111 >> 4 & 1) << PB1)
@@ -127,20 +124,21 @@ void confirmation(){
     PORTC |= (PORTC & ~(0b11111 << PC1)) 
           | ((0b11111 << PC1) & 0xFF);
     _delay_ms(3000);
+    //Ledsausschatung Minutes
     PORTB &= (PORTB & ~(0b111111 << PB0));
     PORTD &= (PORTD & ~(0b11 << PD0) & ~(0b1 << PD7));
-    //Ledsanschatung Hours
+    //Ledsausschatung Hours
     PORTC &= (PORTC & ~(0b11111 << PC1));
 }
 
-// Прерывание INT0 (PD2) — переключение флага сна
+// INT0 (PD2) 
 ISR(INT0_vect){
     sleepflag = !sleepflag;
     if (!sleepflag){
         LEDS();
     }
     else {
-        // Принудительно очистить порты перед сном
+        // Ports clear
         PORTB &= ~(0b111 << PB0);
         PORTD &= ~((1<<PD0)|(1<<PD1)|(1<<PD7));
         PORTC &= ~(0b11111 << PC1);
@@ -148,7 +146,7 @@ ISR(INT0_vect){
 }
 
 ISR(INT1_vect){
-    brightness += 51; // шаг изменения яркости (~20%)
+    brightness += 51; // (~20%)
     if(brightness > 255) brightness = 0;
 }
 
@@ -171,18 +169,17 @@ void software_pwm(){
         if(i < brightness && (zustand_hours & (1<<3))) PORTC |= (1<<PC4); else PORTC &= ~(1<<PC4);
         if(i < brightness && (zustand_hours & (1<<4))) PORTC |= (1<<PC5); else PORTC &= ~(1<<PC5);
 
-        _delay_us(30);  // около 130 Гц
+        _delay_us(30); // fast 130 Hz
     }
 }
 
 
 
 
-void go_sleep(){   // Функция ухода в сон
-    // Вы обнуляете здесь все порты,
+void go_sleep(){   // Sleep modi
+    // Alle ports auf 0 setzen
     PORTB &= (PORTB & ~(0b111111 << PB0));
     PORTD &= (PORTD & ~(0b11 << PD0) & ~(0b1 << PD7));
-    //Ledsanschatung Hours
     PORTC &= (PORTC & ~(0b11111 << PC1));
 
     set_sleep_mode(SLEEP_MODE_PWR_SAVE);
@@ -190,21 +187,35 @@ void go_sleep(){   // Функция ухода в сон
 
     cli();
     sei();
-    sleep_cpu();   // MCU засыпает
-    sleep_disable(); // Возврат после пробуждения
+    sleep_cpu();   // MCU sleep
+    sleep_disable(); // Wieder aufwachen
 }
 
-int main(){
+void time_seting(){
+    if (!(PIND & (1<<PD4))){
+        check_hours();
+        LEDS();
+    }
+    else{
+        Time_addierung();
+        LEDS();
+    }
+}
+
+void ports(){ 
     DDRB |= (1<<PB0)|(1<<PB1)|(1<<PB2);
     DDRC |= (1<<PC1)|(1<<PC2)|(1<<PC3)|(1<<PC4)|(1<<PC5);
     DDRD |= (1<<PD0)|(1<<PD1)|(1<<PD7)|(1<<PD5)|(1<<PD6);
 
-    DDRD &= ~((1<<PD2)|(1<<PD3)|(1<<PD4));
+    DDRD &= ~((1<<PD2)|(1<<PD3)|(1<<PD4));// Konfigurieren Eingänge für Tasten
     PORTD |= (1<<PD2)|(1<<PD3)|(1<<PD4);
 
-    EICRA |= (1<<ISC01)|(1<<ISC11);
-    EIMSK |= (1<<INT0)|(1<<INT1);
+    EICRA |= (1<<ISC01)|(1<<ISC11); // Konfiguriert Triger auf falende Flanke
+    EIMSK |= (1<<INT0)|(1<<INT1); //Maskiert Iterapt0 und 1
+}
 
+int main(){
+    ports();
     start_test();
     timer2_init();
     sei();
@@ -213,11 +224,12 @@ int main(){
         if (!(PIND & (1<<PD4))){
             _delay_ms(50);
             if (!(PIND & (1<<PD4))) { 
-                Time_addierung();
-                LEDS();
-                
+                _delay_ms(1000);
+                time_seting();
             }
-            while (!(PIND & (1<<PD4))) _delay_ms(10);
+
+            while (!(PIND & (1<<PD4))){
+            };
         }
         else if (sleepflag) go_sleep();
         else if (!sleepflag) software_pwm();
